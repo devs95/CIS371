bd205f09a7e5dfae6251e663bc1e4746fab5dc43
devs95 <dsharm@seas.upenn.edu>
2018-02-08 00:02:13 -0500

    Update lc4_alu.v
    finished control flow (mostly)

--- lab2/lc4_alu.v
+++ lab2/lc4_alu.v
@@ -11,50 +11,114 @@ module lc4_alu(input  wire [15:0] i_insn,
                output wire [15:0] o_result);
 
 
-      /*** YOUR CODE HERE ***/
+	
+	wire [3:0] op = i_insn[15:12];
+	wire [2:0] func0 = i_insn[5:3];
+	wire [1:0] func1 = i_insn[8:7];
+	wire func2 = i_insn[11];
+	
+	//signed immediate values
+	wire [8:0] imm9 = i_insn[8:0];
+	wire [6:0] imm7 = i_insn[6:0];
+	wire [5:0] imm6 = i_insn[5:0];
+	wire [4:0] imm5 = i_insn[4:0];
+	
+	//unsigned immediate values
+	wire [3:0] uimm4 = i_insn[3:0];
+	wire [15:0] uimm7 = {8'h00, i_insn[6:0]}; 
+	wire [7:0] uimm8 = i_insn[7:0];
+	
+	//op-code control signals
+	wire is_arith = (op == 4'h1) ? 1'b1 : 1'b0;
+	wire is_log = (op == 4'h5) ? 1'b1 : 1'b0;
+	wire is_compare = (op == 4'h2) ? 1'b1 : 1'b0;
+	wire is_shift = (op == 4'hA) ? 1'b1 : 1'b0;
+	wire is_branch = (op == 4'h0) ? 1'b1 : 1'b0;
+	wire is_jump = (op == 4'hC) ? 1'b1 : 1'b0;
+	wire is_jump_sub = (op == 4'h4) ? 1'b1 : 1'b0;
+	wire is_ldr = (op == 4'h6) ? 1'b1 : 1'b0;
+	wire is_str = (op == 4'h7) ? 1'b1 : 1'b0;
+	wire is_rti = (op == 4'h8) ? 1'b1 : 1'b0;
+	wire is_trap = (op == 4'h15) ? 1'b1 : 1'b0;
+	wire is_const = (op == 4'h9) ? 1'b1: 1'b0;
+	wire is_hiconst = (op == 4'hD) ? 1'b1 : 1'b0;
+	
+	//arithmetic op computations
+	wire [15:0] o_add = i_r1data + i_r2data;
+	wire [15:0] o_mul = i_r1data * i_r2data;
+	wire [15:0] o_sub = i_r1data - i_r2data;
+	// MUST DO DIV!!!!
+	wire [15:0] o_addi = i_r1data + imm5_ext;
+	
+	
+	//arithmetic op muxing
+	wire [15:0] o_arith = 	(func0 == 3'h0) ? o_add :
+							(func0 == 3'h1) ? o_mul :
+							(func0 == 3'h2) ? o_sub :
+							(func0 == 3'h3) ? o_div :
+							(func0[2] == 3'b1) ? o_addi;
+							
+	//logical op computations						
+	wire [15:0] o_and = i_r1data & i_r2data;
+	wire [15:0] o_not = ~i_r1data;
+	wire [15:0] o_or = i_r1data | i_r2data;
+	wire [15:0] o_xor = i_r1data ^ i_r2data;
+	wire [15:0] o_andi = i_r1data & imm5_ext;
+	
+	//logical op muxing						
+	wire [15:0] o_log = 	(func0 == 3'h0) ? o_and :
+							(func0 == 3'h1) ? o_not :
+							(func0 == 3'h2) ? o_or :
+							(func0 == 3'h3) ? o_xor :
+							(func0[2] == 3'b1) ? o_andi;
 
+/***	COMPARE		***/							
+	// MUST DO COMPARE COMPUTATIONS
+
+							
+	//compare op muxing
+	wire [15:0] o_compare = 	(func1 == 2'h0 ? o_cmp :
+								(func1 == 2'h1) ? o_cmpu :
+								(func1 == 2'h2) ? o_cmpi :
+								(func1 == 2'h3) ? o_cmpiu;
+								
+	//shift op muxing							
+	wire [15:0] o_shift = 		(func0[2:1] == 2'h0) ? o_sll :
+								(func0[2:1] == 2'h1) ? o_sra :
+								(func0[2:1] == 2'h2) ? o_srl :
+								(func0[2:1] == 2'h3) ? o_mod;
+								
+/***		JUMP		***/								
+	
+	//jump computations
+	wire [15:0] o_jmpr = i_r1data;
+	// MUST DO JMP
+	
+	//jump op muxing	
+	wire [15:0] o_jump = 		(func2 == 1'b0) ? o_jmpr : o_jmp;
+	
+/***	JUMP TO SUB		***/	
+	//jump to sub computations
+	wire[15:0] o_jsrr = i_r1data;
+	// MUST DO JSR
+	
+	//jump to sub op muxing	
+	wire [15:0] o_jump_sub = 	(func2 == 1'b0) ? o_jsrr : o_jsr;
+	
+	//final output muxing
+	assign o_result = 	is_arith ? o_arith :
+						is_log ? o_log :
+						is_compare ? o_compare :
+						is_shift ? o_shift :
+						is_branch ? o_branch :
+						is_jump ? o_jump :
+						is_jump_sub ? o_jump_sub :
+						is_ldr ? o_ldr :
+						is_str ? o_str :
+						is_rti ? o_rti :
+						is_trap ? o_trap :
+						is_const ? o_const :
+						is_hiconst ? o_hiconst :
+						4'h000;
+	
 endmodule
-
-module lc4_decode(input wire [15:0] i_insn,
-                    output wire [4:0] o_ctrl);
-                    
-    wire [15:0] arith_log_masked = i_insn & 16'hF038; //masked for ADD, MUL, SUB, DIV, ADDI, AND, NOT, OR, XOR, ANDI  
-    wire [15:0] cmp_masked = i_insn & 16'hF180; //masked for CMP, CMPU, CMPI, CMPIU
-    wire [15:0] shift_masked = i_insn & 16'hF030; //masked for SLL, SRA, SRL, MOD
-    wire [15:0] jump_masked = i_insn & 16'hF800; //masked for JSRR, JSR, JMPR, JMP
-    wire [15:0] misc_masked = i_isn & 16'hF000;
-    
-    o_ctrl =    (arith_log_masked == 16'h1000) ? 5'h00 : //ADD
-                (arith_log_masked == 16'h1008) ? 5'h01 : //MUL
-                (arith_log_masked == 16'h1010) ? 5'h02 : //SUB
-                (arith_log_masked == 16'h1018) ? 5'h03 : //DIV
-                ((arith_log_masked & 16'hFF20) == 16'h1020) ? 5'h04 : //ADDI
-                
-                (arith_log_masked == 16'h5000) ? 5'h05 : //AND
-                (arith_log_masked == 16'h5008) ? 5'h06 : //NOT
-                (arith_log_masked == 16'h5010) ? 5'h07 : //OR
-                (arith_log_masked == 16'h5018) ? 5'h08 : //XOR
-                ((arith_log_masked & 16'hFF20) == 16'h5020) ? 5'h09 : //ANDI
-                
-                (cmp_masked == 16'h2000) ? 5'h0A : //CMP
-                (cmp_masked == 16'h2080) ? 5'h0B : //CMPU
-                (cmp_masked == 16'h2100) ? 5'h0C : //CMPI
-                (cmp_masked == 16'h2180) ? 5'h0D : //CMPIU
-                
-                (shift_masked == 16'h8000) ? 5'h0E : //SLL
-                (shift_masked == 16'h8010) ? 5'h0F : //SRA
-                (shift_masked == 16'h8020) ? 5'h10 : //SRL
-                (shift_masked == 16'h8030) ? 5'h11 : //MOD
-                
-                (jump_masked == 16'h4000) ? 5'h12 : //JSRR
-                (jump_masked == 16'h4800) ? 5'h13 : //JSR
-                (jump_masked == 16'hC000) ? 5'h14 : //JMPR
-                (jump_masked == 16'hC800) ? 5'h15 : //JMP
-                
-                (misc_masked == 16'h
-                
-                
-                
-                
-
-end module
