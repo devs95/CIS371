f85b21d5b1a6952489f67c114e6849dc66ed4948
Dev Sharma <dsharm@Sagittarius.seas.upenn.edu>
2018-03-27 15:24:15 -0400

    Finished code. Added stalling and branch logic. Passes all tests, ready to submit

--- lab4/lc4_pipeline.v
+++ lab4/lc4_pipeline.v
@@ -46,35 +46,51 @@ module lc4_processor
     * just restart the simulation.
     */
     
-/****FETCH STAGE****/
+    wire is_stall; //detects load to use dependencies then stalls pipeline (disable we's in F-stage, insert NOP in D-stage)
+    wire is_flush; //detects branch misprediction and inserts NOP's into F and D stages
+    
+    
+/**********************************************FETCH STAGE**********************************************/
 
-    // pc wires attached to the PC register's ports
     wire [15:0] F_pc;      // Current program counter (read out from pc_reg)
-    wire [15:0] F_next_pc; // Next program counter (you compute this and feed it into next_pc)
-    
-    wire [1:0] F_stall = 2'b0; //CHANGE THIS TO SUPPORT STALL/BRANCH
-    
     assign o_cur_pc = F_pc; //read current PC from PC_reg
     
-    //CHANGE THESE TO SUPPORT BRANCH/CONTROL
-    wire [15:0] F_insn = i_cur_insn;
-    assign F_next_pc = F_pc+16'b1; 
+    wire [1:0] F_stall = is_flush ? 2'h2 : 2'b0; //set test_stall signal to indicate flush due to branch
+    wire [15:0] F_insn = is_flush ? 16'h0 : i_cur_insn; //inject NOP on branch misprediction
+    wire [15:0] F_next_pc = is_flush ? X_alu_result : F_pc+16'b1; //mux between PC+1 and branch target
+    
+    wire F_we = ~is_stall; //disable we of all registers in F-stage when stalling
 
     // Program counter register, starts at 8200h at bootup
-    Nbit_reg #(16, 16'h8200) pc_reg (.in(F_next_pc), .out(F_pc), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+    Nbit_reg #(16, 16'h8200) pc_reg (.in(F_next_pc), .out(F_pc), .clk(clk), .we(F_we), .gwe(gwe), .rst(rst));
 
-    //Fetch-Decode Intermediate Register
+/****Fetch-Decode Intermediate Register****/
+
     //FD_reg output wires
     wire [15:0] D_IR_pc;
     wire [15:0] D_IR_insn;
     wire [1:0] D_IR_stall;
     //FD_reg registers
-    Nbit_reg #(16, 16'h0) FD_pc (.in(F_pc), .out(D_IR_pc), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
-    Nbit_reg #(16, 16'h0) FD_insn (.in(F_insn), .out(D_IR_insn), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
-    Nbit_reg #(2, 2'h2) FD_stall (.in(F_stall), .out(D_IR_stall), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+    Nbit_reg #(16, 16'h0) FD_pc (.in(F_pc), .out(D_IR_pc), .clk(clk), .we(F_we), .gwe(gwe), .rst(rst));
+    Nbit_reg #(16, 16'h0) FD_insn (.in(F_insn), .out(D_IR_insn), .clk(clk), .we(F_we), .gwe(gwe), .rst(rst));
+    Nbit_reg #(2, 2'h2) FD_stall (.in(F_stall), .out(D_IR_stall), .clk(clk), .we(F_we), .gwe(gwe), .rst(rst));
     
-/****DECODE STAGE****/
+/**********************************************DECODE STAGE**********************************************/
 
+/****Stall Logic****/
+    wire [2:0] D_stall_r1;
+    wire [2:0] D_stall_r2;
+    wire D_stall_r1re;
+    wire D_stall_r2re;
+    wire D_stall_is_store;
+    wire D_stall_is_branch;
+    //Use parallel decoder to detect stalls because other decoder will be flushed with NOP when a stall is needed. Can't decode stall logic and flush the same decoder in 1 cycle
+    lc4_decoder stall_decoder(.insn(D_IR_insn), .r1sel(D_stall_r1), .r1re(D_stall_r1re), .r2sel(D_stall_r2), .r2re(D_stall_r2re), .wsel(), .regfile_we(), .nzp_we(), .select_pc_plus_one(),
+                            .is_load(), .is_store(D_stall_is_store), .is_branch(D_stall_is_branch), .is_control_insn());
+    
+    //assign stall for load-to-use dependencies, includes load-to-branch, excludes load to store where loaded data is stored
+    assign is_stall = X_IR_is_load & (((D_stall_r1 == X_IR_rd) & D_stall_r1re) || ((D_stall_r2 == X_IR_rd) & D_stall_r2re & ~D_stall_is_store) || D_stall_is_branch);
+    
     //decoder output wires
     wire [2:0] D_r1;
     wire [2:0] D_r2;
@@ -89,26 +105,27 @@ module lc4_processor
     wire D_regfile_we;
     wire D_nzp_we;
     
-    wire [15:0] D_insn = D_IR_insn; //CHANGE TO SUPPORT BRANCH/STALL
+    wire [15:0] D_insn = (is_stall | is_flush) ? 16'h0 : D_IR_insn; //CHANGE TO SUPPORT BRANCH/STALL
     
-    wire [1:0] D_stall = D_IR_stall; //CHNAGE TO SUPPORT BRANCH/STALL
+    wire [1:0] D_stall = is_stall ? 2'h3 : is_flush ? 2'h2 : D_IR_stall; //CHNAGE TO SUPPORT BRANCH/STALL
     
     //decoder
     lc4_decoder decoder(.insn(D_insn), .r1sel(D_r1), .r1re(D_r1re), .r2sel(D_r2), .r2re(D_r2re), .wsel(D_rd), .regfile_we(D_regfile_we), .nzp_we(D_nzp_we), .select_pc_plus_one(D_select_pc_plus_one),
                             .is_load(D_is_load), .is_store(D_is_store), .is_branch(D_is_branch), .is_control_insn(D_is_control_insn));
     
     //regfile output wires
-    
     wire [15:0] D_regfile_r1_data;
     wire [15:0] D_regfile_r2_data;
-    wire [15:0] D_r1_data = (D_r1 == W_IR_rd) & W_IR_regfile_we  ? W_rd_data : D_regfile_r1_data;
+    //WD Bypass
+    wire [15:0] D_r1_data = (D_r1 == W_IR_rd) & W_IR_regfile_we ? W_rd_data : D_regfile_r1_data;
     wire [15:0] D_r2_data = (D_r2 == W_IR_rd) & W_IR_regfile_we ? W_rd_data : D_regfile_r2_data;
     
     
     //regfile
     lc4_regfile regfile(.i_rd(W_IR_rd), .i_rs(D_r1), .i_rt(D_r2), .i_rd_we(W_IR_regfile_we), .i_wdata(W_rd_data), .o_rs_data(D_regfile_r1_data), .o_rt_data(D_regfile_r2_data), .clk(clk) , .gwe(gwe), .rst(rst));
     
-    //Decode-Execute Intermediate Register
+/****Decode-Execute Intermediate Register****/
+
     //DX_reg output wires
     wire [15:0] X_IR_r1_data;
     wire [15:0] X_IR_r2_data;
@@ -149,7 +166,7 @@ module lc4_processor
     Nbit_reg #(16, 16'h0) DX_insn (.in(D_insn), .out(X_IR_insn), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
     Nbit_reg #(2, 2'h2) DX_stall (.in(D_stall), .out(X_IR_stall), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
     
-/****EXECUTE STAGE****/
+/**********************************************EXECUTE STAGE**********************************************/
 
     //WX and MX Bypassing
     wire [15:0] X_r1_data = (M_IR_rd == X_IR_r1) & M_IR_regfile_we ? M_IR_rd_data : (W_IR_rd == X_IR_r1) & W_IR_regfile_we ? W_rd_data : X_IR_r1_data;
@@ -160,7 +177,33 @@ module lc4_processor
     lc4_alu alu(.i_insn(X_IR_insn), .i_pc(X_IR_pc), .i_r1data(X_r1_data), .i_r2data(X_r2_data), .o_result(X_alu_result));
     wire [15:0] X_rd_data = X_IR_select_pc_plus_one ? (X_IR_pc+16'b1) : X_alu_result;
     
-    //Execute-Memory Intermediate Register
+/****NZP/Branch Logic****/
+    //WX and MX bypass for NZP bits
+    wire [15:0] X_nzp_in = M_IR_nzp_we ? M_IR_rd_data : W_rd_data;
+    wire [2:0] X_nzp_new_bits = X_nzp_in[15] ? 3'h4 : (X_nzp_in == 16'h0) ? 3'h2 : 3'h1;
+    wire [2:0] X_nzp_bits = (M_IR_nzp_we | W_IR_nzp_we) ? X_nzp_new_bits : X_nzp_reg_out;
+    //NZP register
+    wire [2:0] X_nzp_reg_out;
+    Nbit_reg #(3, 3'b000) NZP_reg (.in(X_nzp_new_bits), .out(X_nzp_reg_out), .clk(clk), .we(W_IR_nzp_we), .gwe(gwe), .rst(rst));
+    //Branch Logic
+    wire [2:0] X_nzp_se = X_IR_insn[11:9];
+    wire X_p = X_nzp_bits[0];
+    wire X_z = X_nzp_bits[1];
+    wire X_n = X_nzp_bits[2];
+    wire X_br_e =   X_nzp_se == 3'h0 ? 1'b0 :
+                    X_nzp_se == 3'h1 ? X_p :
+                    X_nzp_se == 3'h2 ? X_z :
+                    X_nzp_se == 3'h3 ? X_z | X_p :
+                    X_nzp_se == 3'h4 ? X_n :
+                    X_nzp_se == 3'h5 ? X_n | X_p :
+                    X_nzp_se == 3'h6 ? X_n | X_z :
+                    X_n | X_z | X_p ;
+    
+    assign is_flush = (X_br_e & X_IR_is_branch) | X_IR_is_control_insn; //set flush for taken branches
+                    
+    
+/****Execute-Memory Intermediate Register****/
+
     //XM_reg output wires
     wire [15:0] M_IR_alu_result;
     wire [15:0] M_IR_rd_data;
@@ -206,7 +249,7 @@ module lc4_processor
     Nbit_reg #(2, 2'h2) XM_stall (.in(X_IR_stall), .out(M_IR_stall), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
     
     
-/****MEMORY STAGE****/
+/**********************************************MEMORY STAGE**********************************************/
     
     wire [15:0] M_dmem_addr = (M_IR_is_load | M_IR_is_store) ? M_IR_alu_result : 16'h0; //memory address
     wire [15:0] M_r2_data = (M_IR_r2 == W_IR_rd) & W_IR_regfile_we ? W_rd_data : M_IR_r2_data; //WM Bypass - assign memory write data
@@ -218,7 +261,8 @@ module lc4_processor
     assign o_dmem_we = M_IR_is_store;
     assign o_dmem_addr = M_dmem_addr;
     
-    //Memory-Writeback Intermediate Register
+/****Memory-Writeback Intermediate Register***/
+
     //WM_reg output wires
     wire [15:0] W_IR_dmem_addr;
     wire [15:0] W_IR_dmem_data;
@@ -267,10 +311,11 @@ module lc4_processor
     Nbit_reg #(16, 16'h0) MW_insn (.in(M_IR_insn), .out(W_IR_insn), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
     Nbit_reg #(2, 2'h2) MW_stall (.in(M_IR_stall), .out(W_IR_stall), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
     
-/****WRITEBACK STAGE****/
+/**********************************************WRITEBACK STAGE**********************************************/
     
-    wire [15:0] W_rd_data = (W_IR_is_load) ? W_IR_dmem_data : W_IR_rd_data;
+    wire [15:0] W_rd_data = (W_IR_is_load) ? W_IR_dmem_data : W_IR_rd_data; //select memory data or alu result to write
     
+    //set test wires
     assign test_stall = W_IR_stall;
     assign test_cur_pc = W_IR_pc;
     assign test_cur_insn = W_IR_insn;
@@ -283,11 +328,6 @@ module lc4_processor
     assign test_dmem_addr = W_IR_dmem_addr;
     assign test_dmem_data = (W_IR_is_store) ? W_IR_r2_data : (W_IR_is_load) ? W_IR_dmem_data : 16'h0;
     
-    
-    
-    
-    
-   
 `ifndef NDEBUG
    always @(posedge gwe) begin
       // $display("%d %h %h %h %h %h", $time, f_pc, d_pc, e_pc, m_pc, test_cur_pc);
@@ -303,13 +343,6 @@ module lc4_processor
       // Try adding a $display() call that prints out the PCs of
       // each pipeline stage in hex.  Then you can easily look up the
       // instructions in the .asm files in test_data.
-      
-      /*
-      $display(" ");
-      pinstr(.insn(W_IR_insn));
-      $display(" ");
-      $display("%d PC = %h || r%d = %h || r%d = %h || rd = %d || w_data = %h || regfile_we = ", $time, W_IR_pc, W_IR_r1, W_IR_r1_data, W_IR_r2, W_IR_r2_data, W_IR_rd, W_rd_data, W_IR_regfile_we);
-      */
       
       // basic if syntax:
       // if (cond) begin
